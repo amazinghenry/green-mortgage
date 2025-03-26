f7040bb39a2715c211e6e33426ad207aa2a9878e
AmazingHenry <82852819+amazinghenry@users.noreply.github.com>
2025-03-26 17:43:51 +0000

    bring back firebase

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import GetStartedRibbon from "./components/getstarted/GetStarted";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MortgageCalculator from "./components/mortgagecalculator/MortgageCalculator";
 import Cto from "./components/cto/Cto";
+import SignIn from "./components/signin/SignIn";
 import SubmitDocuments from "./components/submitdocuments/SubmitDocuments";
 import FAQComponent from "./components/faqs/FAQs";
 import NotFound from "./components/notfound/NotFound";
@@ -42,6 +43,15 @@ function App() {
           element={
             <>
               <SubmitDocuments />
+            </>
+          }
+        />
+
+        <Route
+          path="signin"
+          element={
+            <>
+              <SignIn />
             </>
           }
         />
